d0e7c4be58f421f3be925c32451261d3652b6183
Tom <dv.tomc@gmail.com>
2023-03-09 08:56:56 -0700

    day 3 done

---
+++ day3.py
@@ -0,0 +1,61 @@
+
+
+file = open(r"./input3")
+priority_sum = 0
+
+def calculate_prio(a,b):
+    priority = 0
+    for c in a:
+        for d in b:
+            if c == d:
+                if str.islower(c):
+                    priority = ord(c)-96
+                else:
+                    priority = ord(c)-38
+                return priority
+    return priority
+
+def find_badge(a,b,c):
+    priority = 0
+    for d in a:
+        for e in b:
+            if d == e:
+                for f in c:
+                    if d == f:
+                        if str.islower(d):
+                            priority = ord(d)-96
+                        else:
+                            priority = ord(d)-38
+                        return priority
+    return priority
+
+for line in file:    
+    backpack = line.rstrip()
+    
+    a = backpack[:len(backpack)//2]
+    b = backpack[len(backpack)//2:]
+
+    priority_sum += calculate_prio(a,b)
+                
+print(priority_sum)
+file.close()
+
+
+file = open(r"./input3")
+priority_sum = 0
+bag_count = 0
+bags = []
+
+for line in file:    
+    backpack = line.rstrip()
+    
+    bags.append(backpack)
+    bag_count += 1
+
+    if bag_count == 3:
+        priority_sum += find_badge(bags[0], bags[1], bags[2])
+        bag_count = 0
+        bags = []
+
+print(priority_sum)
+file.close()
