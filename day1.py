64dfb8779747f2354e0cd7b6b1daa8ba7f0975fe
Tom <dv.tomc@gmail.com>
2023-03-08 21:51:24 -0700

    day one done

---
+++ day1.py
@@ -0,0 +1,22 @@
+
+
+file = open(r"./input11")
+elf = 1
+elves = {}
+total_cals = 0
+
+for line in file:    
+    cals = line.rstrip()
+    
+    if cals == "":
+        elves[elf] = total_cals
+        total_cals = 0
+        elf += 1
+    else:
+        total_cals += int(cals)
+
+print(max(elves, key=elves.get))
+print(max(elves.values()))
+
+sorted = sorted(elves, key=elves.get, reverse=True)
+print (elves[sorted[0]] + elves[sorted[1]] + elves[sorted[2]])
