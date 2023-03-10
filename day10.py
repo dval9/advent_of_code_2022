7b082c4a7c09d065cd506f01cce82eda9781f4d5
Tom <dv.tomc@gmail.com>
2023-03-10 13:39:47 -0700

    day 10 done

--- day10.py
+++ day10.py
@@ -1,13 +1,45 @@
 
 
-#file = open(r"./input3")
+file = open(r"./input10")
 i = 0
 
-for line in file:    
-    s = line.rstrip()
 
-  
+def update_signals():
+    global cycle
+    pixel = cycle % 40
+
+    if pixel == 0:
+        print('', end='\n')
+
+    if pixel == x or abs(pixel - x) == 1:
+        print('#', end='')
+    else:
+        print('.', end='')
+
+    cycle += 1
+    if (cycle % 20) == 0:
+        signals.append(cycle * x)
+
+    return cycle
 
 
+cycle = 0
+x = 1
+signals = []
+
+for line in file:
+    s = line.rstrip().split(' ')
+
+    if s[0] == 'noop':
+        update_signals()
+    else:
+        update_signals()
+        update_signals()
+        x += int(s[1])
+
+i = signals[0] + signals[2] + signals[4] + \
+    signals[6] + signals[8] + signals[10]
+
+print('', end='\n')
 print(i)
-#file.close()
+file.close()
