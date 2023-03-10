2af1ae7808ff7882b23c88278e3621dbf9a2fdb5
Tom <dv.tomc@gmail.com>
2023-03-09 18:36:40 -0700

    day 6 done

--- day6.py
+++ day6.py
@@ -1,13 +1,36 @@
 
 
-#file = open(r"./input3")
-i = 0
+file = open(r"./input6")
+i = 4
 
 for line in file:    
-    s = line.rstrip()
+    s = list(line.rstrip())
 
-  
+    while len(s) > 4:
 
+        sop = s[:4]
+        if len(sop) == len(set(sop)):
+            break
+        s.pop(0)
+        i += 1
 
 print(i)
-#file.close()
+file.close()
+
+
+file = open(r"./input6")
+i = 14
+
+for line in file:    
+    s = list(line.rstrip())
+
+    while len(s) > 14:
+
+        sop = s[:14]
+        if len(sop) == len(set(sop)):
+            break
+        s.pop(0)
+        i += 1
+
+print(i)
+file.close()
